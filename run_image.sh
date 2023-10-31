44311749816e5cda6ea02d6bb060631255ce04a9
Theideabased <ogunmusireseyi01@gmail.com>
2023-10-31 15:42:15 +0100

    processing

--- run_image.sh
+++ run_image.sh
@@ -4,5 +4,5 @@
 docker build -t my_app:v1 .
 
 # Run my docker image
-docker run -e FLASK_APP=main.py -e FLASK_ENV=development -p 5000:5000 your-image-name
+docker run -e FLASK_APP=main.py -e FLASK_ENV=development -p 5000:5000 my_app
 
